367323eb1cf9a1672f9a34d62def3d88c136c785
colocolov <colocolov.stas@gmail.com>
2025-08-13 12:46:11 +0300

    upd some files

--- src/js/modules/dinam-adap.js
+++ src/js/modules/dinam-adap.js
@@ -1,8 +1,6 @@
 // Dynamic Adapt v.1
 // HTML data-da="where(uniq class name),when(breakpoint),position(digi)"
 // e.x. data-da=".item,992,2"
-// Andrikanych Yevhen 2020
-// https://www.youtube.com/c/freelancerlifestyle
 
 "use strict";
 
